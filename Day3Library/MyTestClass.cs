946d9e2f45e05cacd64529a32b44c7b43a27d68b
Mirolim Majidov <mirolimmajidov@outlook.com>
2024-07-11 11:29:53 +0500

    added new project

---
+++ Day3Library/MyTestClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Day3Library;
+
+public class MyTestClass
+{
+    private int Pro1 { get; set; }
+    public int Pro2 { get; protected set; }
+    internal int Pro3 { get; set; }
+    public int Pro4 { get; private set; }
+}
