592474def17f063516403d729643fb424b51ff7e
Mirolim Majidov <mirolimmajidov@outlook.com>
2024-08-14 11:56:15 +0500

    Created a few projects for testing

---
+++ Alif.NUnit.UnitTests.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Alif.NUnit.UnitTests.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
---
+++ Alif.NUnit.UnitTests.Moq/Calculator.cs
@@ -0,0 +1,52 @@
+﻿public interface ILogger
+{
+    public string Type { get; set; }
+    void Log(string message);
+
+    public string GetType()
+    {
+        return Type;
+    }
+}
+
+public class Calculator
+{
+    private readonly ILogger _logger;
+    public int maxNumber = 5;
+
+    public Calculator(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public int Add(int a, int b)
+    {
+        _logger.Log($"Adding {a} and {b}");
+        _logger.Log($"Adding {a} and {b}");
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        _logger.Log($"Subtracting {b} from {a}");
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        _logger.Log($"Multiplying {a} and {b}");
+        return a * b;
+    }
+
+    public double Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            _logger.Log("Attempted to divide by zero");
+            throw new ArgumentException("Cannot divide by zero.");
+        }
+
+        _logger.Log($"Dividing {a} by {b}");
+        return (double)a / b;
+    }
+}
